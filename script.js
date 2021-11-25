964ec8832a2959af7ae9880f0c95510605640adb
vivek bose <vivekbose2013@gmail.com>
2021-11-25 13:06:17 +0530

    initial

---
+++ script.js
@@ -0,0 +1,27 @@
+// function displayValue(num){
+//     let res=document.querySelector("#result")
+//     res.value+=num
+
+// }
+// function clearBox(){
+//     document.querySelector("#result").value=""
+// }
+// function operationB(){
+//     let res=document.querySelector("#result")
+//     let cur_val=res.value
+//     let result=eval(cur_val)
+//     res.value=result
+
+// }
+// function back(){
+//     var text=document.getElementById("#result")
+//     let cur_value=res.value
+//     res.value=cur_value.slice(0,-1)
+// }
+
+var displayValue=(num)=>result.value+=num
+
+var clearBox=()=>result.value=""
+
+var operationB=()=>result.value=eval(result.value)
+
